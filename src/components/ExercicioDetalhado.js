d4404f98f0af7586b9df5d2ac4f67b6fdc9968fd
Rodrigo Martinez <101828320+Digo0101@users.noreply.github.com>
2024-12-04 03:46:47 +0000

    11 comit

--- src/components/ExercicioDetalhado.js
+++ src/components/ExercicioDetalhado.js
@@ -6,31 +6,19 @@ function ExercicioDetalhado() {
   const { id } = useParams();
   const navigate = useNavigate();
 
-  // Converte o id para um número inteiro para garantir que o acesso ao array esteja correto
   const exercicioId = parseInt(id, 10);
-
-  // Pega o exercício correspondente ao id
   const exercicio = ListadeExercicios[exercicioId];
 
-  // Sempre chama o hook useState
   const [atividadeRegistrada, setAtividadeRegistrada] = useState(false);
 
-  // Se o exercício não for encontrado, mostra uma mensagem
   if (!exercicio) {
     return <p>Exercício não encontrado</p>;
   }
 
-  // Função para registrar a atividade
   const registrarAtividade = () => {
     setAtividadeRegistrada(true);
-
-    // Recupera atividades registradas ou cria uma nova lista
     const atividadesRegistradas = JSON.parse(localStorage.getItem('atividadesRegistradas')) || [];
-    
-    // Adiciona o exercício à lista de atividades
     atividadesRegistradas.push(exercicio);
-
-    // Salva a lista atualizada no localStorage
     localStorage.setItem('atividadesRegistradas', JSON.stringify(atividadesRegistradas));
   };
 
@@ -42,7 +30,7 @@ function ExercicioDetalhado() {
         width="560" 
         height="315" 
         src={exercicio.videoUrl} 
-        title="YouTube video player" 
+        title={exercicio.titulo} 
         frameBorder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
         allowFullScreen
--- src/components/data/exerciciosData.js
+++ src/components/data/exerciciosData.js
@@ -1,70 +1,68 @@
-// src/data/exerciciosData.js
 export const ListadeExercicios = [
   { 
     titulo: "Caminhada", 
     imgSrc: "/images/caminhada2.jpg", 
     descricao: "A caminhada é uma atividade física simples e eficaz, ideal para iniciantes.",
-    videoUrl: "https://youtu.be/MIYuqCn7gKI?si=2JHI3Nz-_b2_8SIl"
+    videoUrl: "https://www.youtube.com/embed/MIYuqCn7gKI"
   },
   { 
     titulo: "Supino", 
     imgSrc: "/images/supino.jpg.webp", 
     descricao: "O supino é um exercício clássico para trabalhar o peito e os tríceps.",
-    videoUrl: "https://youtu.be/EZMYCLKuGow?si=e4oFWwBxv9sJPP-n"
+    videoUrl: "https://www.youtube.com/embed/EZMYCLKuGow"
   },
   {
     titulo: "Polichinelo", 
     imgSrc: "/images/poli.jpg", 
-    descricao: "Polichinelo pode ser uma opção caso você não tenha corda. Fique de pé, junte as pernas e apoie os braços ao lado do corpo. Depois, basta pular com os braços estendidos e, ao mesmo tempo, afastar os pés de acordo com a distância entre os ombros e bater as mãos acima da cabeça. Repita a sequência por 15 a 20 vezes.",
-    videoUrl: "https://youtu.be/0qjvdP9-MBU?si=gYcF682w5FAmUcoR"
+    descricao: "Polichinelo pode ser uma opção caso você não tenha corda...",
+    videoUrl: "https://www.youtube.com/embed/0qjvdP9-MBU"
   },
   {
-    titulo: "Flexao de Braço", 
+    titulo: "Flexão de Braço", 
     imgSrc: "/images/flexao.jpg", 
-    descricao: "Deite-se com a barriga para baixo e apoie mãos e pés no chão. Se você for iniciante, use os joelhos para apoiar. Afaste as mãos em uma distância maior do que a dos ombros. Com as costas alinhadas, flexione os braços e desça o corpo sem tocar o peito no chão. Repita o processo até 10 vezes e faça três séries iguais com alguns segundos de descanso entre cada uma.",
-    videoUrl: "https://youtu.be/dHgoYiCraCw?si=JumGezvuc1IYeMao"
+    descricao: "Deite-se com a barriga para baixo e apoie mãos e pés no chão...",
+    videoUrl: "https://www.youtube.com/embed/dHgoYiCraCw"
   },
   {
-    titulo: "Abidominal ", 
+    titulo: "Abdominal", 
     imgSrc: "/images/abido.jpg", 
-    descricao: "Para fazê-la, deite-se com a barriga para cima, flexione as pernas, apoie os pés no chão e coloque as mãos na nuca ou cruze os braços no peito fazendo um “X”. Eleve o tronco em direção aos joelhos e repita o movimento por pelo menos 10 vezes. É recomendado fazer cerca de três séries iguais com 30 segundos de descanso entre cada uma. Para quem treina há mais tempo, a sequência pode ter 20 repetições ou mais.",
-    videoUrl: "https://youtu.be/6QtWYtKkrGg?si=SK8DeYel3347b44z"
+    descricao: "Para fazê-la, deite-se com a barriga para cima, flexione as pernas...",
+    videoUrl: "https://www.youtube.com/embed/6QtWYtKkrGg"
   },
   {
     titulo: "Leg-Press", 
     imgSrc: "/images/leg.jpg", 
-    descricao: "Leg press é um equipamento de academia que trabalha boa parte dos músculos inferiores do corpo. Isto é, o aparelho propõe a flexão e a extensão das pernas com a pessoa sentada na máquina, levantando um peso definido de forma individual.",
-    videoUrl: "https://youtu.be/nY8UsiAqwds?si=pbB-vvcbuCDhrYRM"
+    descricao: "Leg press é um equipamento de academia que trabalha boa parte dos músculos inferiores...",
+    videoUrl: "https://www.youtube.com/embed/nY8UsiAqwds"
   },
   {
     titulo: "Agachamento", 
     imgSrc: "/images/agacha.jpg", 
-    descricao: "Agachamentos são exercícios de força que estimulam os músculos das pernas, dos quadris e dos glúteos. Eles ajudam a melhorar a resistência muscular, a estabilidade do core e a flexibilidade das articulações dos quadris e joelhos. Os agachamentos também são eficazes na tonificação muscular e no aumento da força nas pernas.",
-    videoUrl: "https://youtu.be/s7i94Okznns?si=BbG94MbBvdVQiE2b"
+    descricao: "Agachamentos são exercícios de força que estimulam os músculos das pernas...",
+    videoUrl: "https://www.youtube.com/embed/s7i94Okznns"
   },
   {
     titulo: "Pula corda", 
     imgSrc: "/images/corda.jpg", 
-    descricao: "Pular corda é um exercício intenso. Por causa disso, o coração tende a trabalhar mais durante a prática, assim como também aumenta a frequência respiratória.",
-    videoUrl: "https://youtu.be/kBF9Upj-h9o?si=RJGQQW5Vs8el976v"
+    descricao: "Pular corda é um exercício intenso que aumenta a frequência respiratória...",
+    videoUrl: "https://www.youtube.com/embed/kBF9Upj-h9o"
   },
   {
     titulo: "Prancha", 
     imgSrc: "/images/prancha.jpg", 
-    descricao: "A prancha é um exercício de isometria que fortalece o core, incluindo os músculos abdominais, lombares e oblíquos. Além de melhorar a força central, também ajuda a melhorar a postura e a estabilidade da coluna vertebral, reduzindo o risco de lesões nas costas.",
-    videoUrl: "https://youtu.be/qNRqGqESAWU?si=1kgyubQDXLiXPVIJ"
+    descricao: "A prancha é um exercício de isometria que fortalece o core...",
+    videoUrl: "https://www.youtube.com/embed/qNRqGqESAWU"
   },
   {
     titulo: "Burpess", 
     imgSrc: "/images/burpees.jpg", 
-    descricao: "Os burpees são exercícios de corpo inteiro combinando agachamento, flexão e salto. Eles são excelentes para desenvolver a resistência cardiovascular, a força muscular e a coordenação. São desafiantes, mas seus benefícios são notáveis, incluindo a queima de calorias e o fortalecimento de múltiplos grupos musculares.",
-    videoUrl: "https://youtu.be/QyuQSvEuzAc?si=K2Dyq0Z0_KCGGUxB"
+    descricao: "Os burpees são exercícios de corpo inteiro combinando agachamento, flexão e salto...",
+    videoUrl: "https://www.youtube.com/embed/QyuQSvEuzAc"
   },
   {
     titulo: "Dança", 
     imgSrc: "/images/danca.jpg", 
-    descricao: "Música e movimento corporal têm tudo a ver com atividade física. Existem diversos ritmos e modalidades de dança para escolher, o que depende do gosto e das habilidades da pessoa. Uma sugestão é procurar vídeos específicos na internet e seguir os passos indicados",
-    videoUrl: "https://youtu.be/otDVwrGnZus?si=CyDOH8J1-tPd7epv"
-  },
-  // Adicione mais exercícios aqui
+    descricao: "Música e movimento corporal têm tudo a ver com atividade física...",
+    videoUrl: "https://www.youtube.com/embed/otDVwrGnZus"
+  }
 ];
